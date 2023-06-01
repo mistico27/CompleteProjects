05db84cc4ad97ebf8f660e99aea352d7a771d6ce
chris_beltran <cristianb72@hotmail.com>
2023-05-31 18:55:09 -0600

    Crudnodjex

---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+///Register
+router.post("/register", async (req,res)=>{
+    
+    try{///create new Password
+        const salt=await bcrypt.genSalt(10);
+        const hasPassword = await bcrypt.hash(req.body.password,salt);
+        const newUser = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hasPassword
+        })
+        //save user and response
+        const user = await newUser.save();
+        res.status(200).json(user);
+    }catch(err){
+        console.log(err);
+    }
+
+});
+
+///login
+router.post("/login", async(req,res)=>{
+    try{
+    const user = await User.findOne({email:req.body.email});
+    !user && res.status(404).json("User not found");
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    console.log(validPassword);
+    !validPassword && res.status(400).json("Wrong password");
+    res.status(200).json(user);
+    }catch(err){
+        console.log(err);
+    }
+});
+
+
+module.exports =router;
