4d3ddea5eb34e21605c7f54bc03997eb3952790f
chris_beltran <cristianb72@hotmail.com>
2023-06-01 17:32:30 -0600

    ReactSocialFront

---
+++ react-app/src/pages/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
+import RightBar from '../../components/rightbar/rightbar'
+import Sidebar from '../../components/sidebar/sidebar'
+
+export default function Home(){
+return(
+    <>
+    <Topbar/>
+    <RightBar/>
+    <Sidebar/>
+    </>
+)
+
+}
