17b123a179918dfe4bcc95b5d6c450dbd683bc65
chris_beltran <cristianb72@hotmail.com>
2023-06-01 02:53:08 -0600

    mistezys

--- routes/users.js
+++ routes/users.js
@@ -89,6 +89,29 @@ router.put("/:id/follow",async(req,res)=>{
 
 
 ///unfollow user
+router.put("/:id/unfollow",async(req,res)=>{
+    if(req.body.userId !== req.params.id){
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(user.followers.includes(req.body.userId)){
+                await user.updateOne({$pull:{ followers:req.body.userId}});
+                await currentUser.updateOne({$pull:{ followins:req.body.userId}});
+                res.status(200).json("user has been unfollowed");
+            }else{
+                res.status(403).json("you do not follow this user");
+            }
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(500).json("you can unfollow to yourself");
+    }
+
+
+});
+
+
 
 
 router.get("/",(req,res)=>{
